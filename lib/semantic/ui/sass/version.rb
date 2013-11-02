67b184649c7fd2a5674e24d4b818fb25e972801d
doabit <doinsist@gmail.com>
2013-11-03 02:49:25 +0800

    Bump version 0.8.6.0

--- lib/semantic/ui/sass/version.rb
+++ lib/semantic/ui/sass/version.rb
@@ -1,8 +1,8 @@
 module Semantic
   module Ui
     module Sass
-      VERSION = "0.8.5.0"
-      SEMANTIC_UI_SHA = 'a9502e4dadc0e766030b362ed2a65fba7176800d'
+      VERSION = "0.8.6.0"
+      SEMANTIC_UI_SHA = '64bdb9f5fad4e94853a68e061e01ba92a90829d6'
     end
   end
 end
