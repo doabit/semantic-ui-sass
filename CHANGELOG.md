c5c14eaeafb395dd2d8ed2f9a8bc92f2416d3e31
doabit <doinsist@gmail.com>
2014-02-21 13:02:24 +0800

    Bump version 0.13.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.13.0.0
+
+* Update Semantic UI to 0.13.0
+
 ## 0.12.5.0
 
 * Update Semantic UI to 0.12.5
--- lib/semantic/ui/sass/version.rb
+++ lib/semantic/ui/sass/version.rb
@@ -1,8 +1,8 @@
 module Semantic
   module Ui
     module Sass
-      VERSION = "0.12.5.0"
-      SEMANTIC_UI_SHA = 'a6bb85bf6dae76b21e859ca935e35e1fbede9040'
+      VERSION = "0.13.0.0"
+      SEMANTIC_UI_SHA = 'fb014b96a85147bfc37cf4c75b08ae1b4f5c2122'
     end
   end
 end
