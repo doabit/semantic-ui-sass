e5d937bd29cd8fc64cc19b2850fab91114a21396
jaredmoody <jared@jaredmoody.com>
2014-10-12 00:15:28 -0700

    Fix all deprecation warnings when running specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
 
 RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
