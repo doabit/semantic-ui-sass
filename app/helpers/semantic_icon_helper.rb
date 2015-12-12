5ef34d34a67fb9475bca74894ba29ac2b3b7e951
doabit <doinsist@gmail.com>
2015-12-13 03:07:06 +0800

    Add id attr to semantic_icon helper

--- app/helpers/semantic_icon_helper.rb
+++ app/helpers/semantic_icon_helper.rb
@@ -1,5 +1,5 @@
 module SemanticIconHelper
-  def semantic_icon(*names)
-    content_tag :i, nil, class: names.map{|name| "#{name.to_s.gsub('_','-')}" } << 'icon'
+  def semantic_icon(*names, id: nil)
+    content_tag :i, nil, class: names.map{|name| "#{name.to_s.gsub('_','-')}" } << 'icon', id: id
   end
 end
--- spec/helpers/semantic_icon_helper_spec.rb
+++ spec/helpers/semantic_icon_helper_spec.rb
@@ -17,4 +17,20 @@ describe SemanticIconHelper do
     expect(semantic_icon(:add, :sign)).to eq '<i class="add sign icon"></i>'
   end
 
+  it "render icon with name and id" do
+    expect(semantic_icon('add', id: 'id')).to eq '<i class="add icon" id="id"></i>'
+  end
+
+  it "render icon with names with string and id" do
+    expect(semantic_icon('add sign', id: 'id')).to eq '<i class="add sign icon" id="id"></i>'
+  end
+
+  it "render icon with names with params and id" do
+    expect(semantic_icon('add', 'sign', id: 'id')).to eq '<i class="add sign icon" id="id"></i>'
+  end
+
+  it "render icon with names with sym params and id" do
+    expect(semantic_icon(:add, :sign, id: 'id')).to eq '<i class="add sign icon" id="id"></i>'
+  end
+
 end
