f8955295253da488fc869b4f1bb6f3d35b58d298
Håkan Nylén <confacted@gmail.com>
2016-05-12 16:17:14 +0200

    fixed rails 5 error

--- lib/semantic/ui/sass/engine.rb
+++ lib/semantic/ui/sass/engine.rb
@@ -5,7 +5,11 @@ module Semantic
       module Rails
         class Engine < ::Rails::Engine
           initializer "semantic-ui-sass.assets.precompile" do |app|
-            app.config.assets.precompile << %r(semantic-ui\/(basic\.)*icons\.(?:eot|svg|ttf|woff)$)
+            if ::Rails.version >= '5'
+              ::Rails.application.config.assets.precompile += %w{ semantic-ui/*icons.* }
+            else
+              app.config.assets.precompile << %r(semantic-ui\/(basic\.)*icons\.(?:eot|svg|ttf|woff)$)
+            end
           end
           initializer 'semantic-ui-sass.setup_helpers' do |app|
             app.config.to_prepare do
