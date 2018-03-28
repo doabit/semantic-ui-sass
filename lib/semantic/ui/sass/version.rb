6df983ea3267bc50cccf6c6a077949d3ea160993
AlecksJohannes <alecksjohanssen@gmail.com>
2018-03-28 12:27:01 +0700

    Update to the latest version 2.3.1

--- lib/semantic/ui/sass/version.rb
+++ lib/semantic/ui/sass/version.rb
@@ -1,8 +1,8 @@
 module Semantic
   module Ui
     module Sass
-      VERSION = "2.3.0.0"
-      SEMANTIC_UI_SHA = 'b211005de65d77a2f45bda3516eb82cc4fdca703'
+      VERSION = "2.3.1"
+      SEMANTIC_UI_SHA = '6dbc09a8737b691bb3d3314e068570c728d2df9f'
     end
   end
 end
