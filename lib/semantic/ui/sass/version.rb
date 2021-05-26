c33e294abb738bcc52217d462be84cb218db9af1
doabit <doinsist@gmail.com>
2021-05-26 08:40:18 +0800

    chore(release): publish 2.4.3.0

--- lib/semantic/ui/sass/version.rb
+++ lib/semantic/ui/sass/version.rb
@@ -1,8 +1,8 @@
 module Semantic
   module Ui
     module Sass
-      VERSION = "2.4.2.0"
-      SEMANTIC_UI_SHA = '3989d7243c514f9018a55eb53cb34daf3bbf5a26'
+      VERSION = "2.4.3.0"
+      SEMANTIC_UI_SHA = "3989d7243c514f9018a55eb53cb34daf3bbf5a26"
     end
   end
 end
