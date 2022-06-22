513e4a2665096367c6105b4aec5d2058d4953251
Sean Deng <doinsist@gmail.com>
2022-06-22 20:25:29 +0800

    chore(release): publish 2.5.0.0

--- lib/semantic/ui/sass/version.rb
+++ lib/semantic/ui/sass/version.rb
@@ -1,7 +1,7 @@
 module Semantic
   module Ui
     module Sass
-      VERSION = "2.4.4.0"
+      VERSION = "2.5.0.0"
       SEMANTIC_UI_SHA = "3989d7243c514f9018a55eb53cb34daf3bbf5a26"
     end
   end
